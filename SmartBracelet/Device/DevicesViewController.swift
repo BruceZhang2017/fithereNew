27f060bfd78d7a45147fa61c30f62f4ae5173427
bruce.zhang <bruce.zhang@anker-in.com>
2025-08-09 21:10:37 +0800

    添加卡包

--- SmartBracelet/Device/DevicesViewController.swift
+++ SmartBracelet/Device/DevicesViewController.swift
@@ -337,7 +337,7 @@ class DevicesViewController: BaseViewController, UIDocumentInteractionController
     
     public func refreshHeight() {
         // 将 deviceSettingsViewHeightMultiplier 修改为 14
-        deviceSettingsViewHeightMultiplier = 14
+        deviceSettingsViewHeightMultiplier = 15
             
         deviceSettingsView?.view.snp.remakeConstraints {
             $0.left.equalTo(0)
