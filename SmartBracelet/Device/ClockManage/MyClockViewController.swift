951f5d4868659362fd03c7d505ab826fb8b4dcb9
bruce.zhang <bruce.zhang@anker-in.com>
2025-12-18 11:45:26 +0800

    修改多语言文案

--- SmartBracelet/Device/ClockManage/MyClockViewController.swift
+++ SmartBracelet/Device/ClockManage/MyClockViewController.swift
@@ -30,7 +30,14 @@ class MyClockViewController: UIViewController {
     var colorIndex = 0 ///白色0 黑色1 黄色2 橙色3 粉色4 紫色5 蓝色6 青色7
     var diallocation = 5
     final let locations = ["above".localized(), "below".localized()]
-    final let xgztlocations = ["无".localized(), "左上".localized(), "左下".localized(), "右上".localized(), "右下".localized(), "居中".localized()]
+    final let xgztlocations = [
+        "position_none".localized(),
+        "position_top_left".localized(),
+        "position_bottom_left".localized(),
+        "position_top_right".localized(),
+        "position_bottom_right".localized(),
+        "position_center".localized()
+    ]
     final let tops = ["closure".localized(), "date".localized(), "sleep".localized(), "heart_rate".localized(), "step".localized()]
     var topTap = false
     final var colors: [UIColor] = [UIColor.white, UIColor.black, UIColor.yellow,
@@ -686,7 +693,7 @@ extension MyClockViewController: UITableViewDataSource {
             cell.topLC.constant = datetimeLocation == 0 ? 10 : 74
             cell.itemImageView.contentMode = .scaleToFill
             cell.itemImageView.backgroundColor = UIColor.gray
-            cell.itemImageView.layer.cornerRadius = 30
+            cell.itemImageView.layer.cornerRadius = AppDelegate.IsDeviceNotRound() ? 30 : width / 2
             cell.itemImageView.clipsToBounds = true
             cell.selectButton.setTitle("select_image".localized(), for: .normal)
             cell.selectButton.setTitleColor(UIColor.brand, for: .normal)
@@ -803,7 +810,11 @@ extension MyClockViewController: TZImagePickerControllerDelegate {
         imageUploadVc?.modalTransitionStyle = .crossDissolve
         imageUploadVc?.view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
         imageUploadVc?.delegate = self
-        imageUploadVc?.image = photos.first
+        if isXGZT && !AppDelegate.IsDeviceNotRound() {
+            imageUploadVc?.image = photos.first?.croppedToCircleSmooth()
+        } else {
+            imageUploadVc?.image = photos.first
+        }
         imageUploadVc?.imgView.contentMode = .scaleAspectFit
         self.present(imageUploadVc!, animated: false) {
             
